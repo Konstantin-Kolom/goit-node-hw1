217267d4aba5ca5108b46583b4300c554cd309ce
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-11-15 17:04:56 +0200

    готво

---
+++ index.js
@@ -0,0 +1,37 @@
+const argv = require('yargs').argv;
+const contactsOperations = require('./contacts');
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const contacts = await contactsOperations.listContacts();
+      console.log(contacts);
+      break;
+
+    case 'get':
+      const getContactById = await contactsOperations.getContactById(id);
+      console.log(getContactById);
+
+      break;
+
+    case 'add':
+      const addtContact = await contactsOperations.addContact(name, email, phone);
+      console.log(addtContact);
+      break;
+
+    case 'remove':
+      const removeContact = await contactsOperations.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+};
+
+invokeAction(argv);
+
+// node index.js --action list
+// node index.js --action get --id 5
+// node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+// node index.js --action remove --id=3
